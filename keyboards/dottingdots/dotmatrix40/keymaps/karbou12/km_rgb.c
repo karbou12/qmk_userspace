20f77b9dbb9e7a9c58e331433511e868a5866930
Karbou <201307916+karbou12@users.noreply.github.com>
2025-12-06 16:25:34 +0900

    Refactored to move km_rgb_layers definition.

--- keyboards/dottingdots/dotmatrix40/keymaps/karbou12/km_rgb.c
+++ keyboards/dottingdots/dotmatrix40/keymaps/karbou12/km_rgb.c
@@ -16,20 +16,6 @@ const rgblight_segment_t PROGMEM km_layer9_layer[]   = RGBLIGHT_LAYER_SEGMENTS({
 const rgblight_segment_t PROGMEM km_capsword_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 6, HSV_RED});
 const rgblight_segment_t PROGMEM km_layerOFF_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 6, HSV_OFF});
 
-#ifdef RGBLIGHT_LAYER_BLINK
-const rgblight_segment_t PROGMEM km_reset_layer[] =       RGBLIGHT_LAYER_SEGMENTS({0, 6, HSV_WHITE});
-const rgblight_segment_t PROGMEM km_turn_on_layer[] =     RGBLIGHT_LAYER_SEGMENTS({0, 6, HSV_GOLD});
-const rgblight_segment_t PROGMEM km_turn_off_layer[] =    RGBLIGHT_LAYER_SEGMENTS({0, 6, HSV_RED});
-const rgblight_segment_t PROGMEM km_set_default_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 6, HSV_GREEN});
-
-const rgblight_segment_t * const PROGMEM km_blink_layers[] = RGBLIGHT_LAYERS_LIST(
-    km_reset_layer,
-    km_turn_on_layer,
-    km_turn_off_layer,
-    km_set_default_layer
-);
-#endif
-
 const rgblight_segment_t * const PROGMEM km_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     km_layer0_layer,
     km_layer1_layer,
@@ -43,4 +29,18 @@ const rgblight_segment_t * const PROGMEM km_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     km_layer9_layer,
     km_capsword_layer
 );
+
+#ifdef RGBLIGHT_LAYER_BLINK
+const rgblight_segment_t PROGMEM km_reset_layer[] =       RGBLIGHT_LAYER_SEGMENTS({0, 6, HSV_WHITE});
+const rgblight_segment_t PROGMEM km_turn_on_layer[] =     RGBLIGHT_LAYER_SEGMENTS({0, 6, HSV_GOLD});
+const rgblight_segment_t PROGMEM km_turn_off_layer[] =    RGBLIGHT_LAYER_SEGMENTS({0, 6, HSV_RED});
+const rgblight_segment_t PROGMEM km_set_default_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 6, HSV_GREEN});
+
+const rgblight_segment_t * const PROGMEM km_blink_layers[] = RGBLIGHT_LAYERS_LIST(
+    km_reset_layer,
+    km_turn_on_layer,
+    km_turn_off_layer,
+    km_set_default_layer
+);
+#endif
 #endif
