0fa678be6e8b0491ec2a36b036d608840d792b9e
Karbou <201307916+karbou12@users.noreply.github.com>
2025-12-06 21:22:06 +0900

    Modified cornelius keymap.

--- keyboards/foostan/cornelius/rev2/keymaps/karbou12/keymap.c
+++ keyboards/foostan/cornelius/rev2/keymaps/karbou12/keymap.c
@@ -1,31 +1,90 @@
+/* Copyright 2020 foostan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+// Copyright 2025 Tano Karbou (github: karbou12 / X: @karbou_12)
+// SPDX-License-Identifier: GPL-2.0-or-later
 #include QMK_KEYBOARD_H
+#include "us_keymap.h"
+#include "us_keycodes.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,   KC_P,  KC_BSPC,
-       KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,     KC_K,    KC_L, KC_SCLN, KC_QUOT,
-       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,  KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-        KC_ESC, KC_LCTL, KC_LGUI, KC_LALT, TL_LOWR,  KC_SPC,  KC_ENT, TL_UPPR,  KC_RALT, KC_RGUI, KC_RCTL,  KC_ESC
+     /*
+      * ┌───┬───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┬───┐
+      * │   │   │   │   │   │   │   │   │   │   │   │   │   │
+      * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+      * │   │   │   │   │   │   │   │   │   │   │   │   │   │
+      * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+      * │   │   │   │   │   │   │   │   │   │   │   │   │   │
+      * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+      * │   │   │   │   │   │   │   │   │   │   │   │   │   │
+      * └───┴───┴───┴───┴───┴───┘   └───┴───┴───┴───┴───┴───┘
+      */
+
+    [0] = LAYOUT_wrapper(
+        MAC_NOR40_BASE_LEFT_TOP,                     MAC_NOR40_BASE_RIGHT_TOP,
+        MAC_NOR40_BASE_LEFT_MIDDLE,                  MAC_NOR40_BASE_RIGHT_MIDDLE,
+        MAC_NOR40_BASE_LEFT_BOTTOM,                  MAC_NOR40_BASE_RIGHT_BOTTOM,
+        QAZ_NONE_THUMBS, MAC_QAZ_BASE_LEFT_THUMBS,   MAC_QAZ_BASE_RIGHT_THUMBS, QAZ_NONE_THUMBS
     ),
 
-    [1] = LAYOUT(
-       _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
-       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
-       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    [1] = LAYOUT_wrapper(
+        MAC_NOR40_RAISE_LEFT_TOP,                     MAC_NOR40_RAISE_RIGHT_TOP,
+        MAC_NOR40_RAISE_LEFT_MIDDLE,                  MAC_NOR40_RAISE_RIGHT_MIDDLE,
+        MAC_NOR40_RAISE_LEFT_BOTTOM,                  MAC_NOR40_RAISE_RIGHT_BOTTOM,
+        QAZ_NONE_THUMBS, MAC_QAZ_RAISE_LEFT_THUMBS,   MAC_QAZ_RAISE_RIGHT_THUMBS, QAZ_NONE_THUMBS
     ),
 
-    [2] = LAYOUT(
-       _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS,  KC_EQL, KC_LCBR, KC_RCBR, KC_PIPE,  KC_GRV,
-       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, KC_TILD,
-       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    [2] = LAYOUT_wrapper(
+        MAC_NOR40_LOWER_LEFT_TOP,                     MAC_NOR40_LOWER_RIGHT_TOP,
+        MAC_NOR40_LOWER_LEFT_MIDDLE,                  MAC_NOR40_LOWER_RIGHT_MIDDLE,
+        MAC_NOR40_LOWER_LEFT_BOTTOM,                  MAC_NOR40_LOWER_RIGHT_BOTTOM,
+        QAZ_NONE_THUMBS, MAC_QAZ_LOWER_LEFT_THUMBS,   MAC_QAZ_LOWER_RIGHT_THUMBS, QAZ_NONE_THUMBS
     ),
 
-    [3] = LAYOUT(
-       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    [3] = LAYOUT_wrapper(
+        MAC_NOR40_ADJUST_LEFT_TOP,                     MAC_NOR40_ADJUST_RIGHT_TOP,
+        MAC_NOR40_ADJUST_LEFT_MIDDLE,                  MAC_NOR40_ADJUST_RIGHT_MIDDLE,
+        MAC_NOR40_ADJUST_LEFT_BOTTOM,                  MAC_NOR40_ADJUST_RIGHT_BOTTOM,
+        QAZ_NONE_THUMBS, MAC_QAZ_ADJUST_LEFT_THUMBS,   MAC_QAZ_ADJUST_RIGHT_THUMBS, KC_NO, KC_NO, QK_BOOT
+    ),
+
+    [4] = LAYOUT_wrapper(
+        WIN_NOR40_BASE_LEFT_TOP,                     WIN_NOR40_BASE_RIGHT_TOP,
+        WIN_NOR40_BASE_LEFT_MIDDLE,                  WIN_NOR40_BASE_RIGHT_MIDDLE,
+        WIN_NOR40_BASE_LEFT_BOTTOM,                  WIN_NOR40_BASE_RIGHT_BOTTOM,
+        QAZ_NONE_THUMBS, WIN_QAZ_BASE_LEFT_THUMBS,   WIN_QAZ_BASE_RIGHT_THUMBS, QAZ_NONE_THUMBS
+    ),
+
+    [5] = LAYOUT_wrapper(
+        WIN_NOR40_RAISE_LEFT_TOP,                     WIN_NOR40_RAISE_RIGHT_TOP,
+        WIN_NOR40_RAISE_LEFT_MIDDLE,                  WIN_NOR40_RAISE_RIGHT_MIDDLE,
+        WIN_NOR40_RAISE_LEFT_BOTTOM,                  WIN_NOR40_RAISE_RIGHT_BOTTOM,
+        QAZ_NONE_THUMBS, WIN_QAZ_RAISE_LEFT_THUMBS,   WIN_QAZ_RAISE_RIGHT_THUMBS, QAZ_NONE_THUMBS
+    ),
+
+    [6] = LAYOUT_wrapper(
+        WIN_NOR40_LOWER_LEFT_TOP,                     WIN_NOR40_LOWER_RIGHT_TOP,
+        WIN_NOR40_LOWER_LEFT_MIDDLE,                  WIN_NOR40_LOWER_RIGHT_MIDDLE,
+        WIN_NOR40_LOWER_LEFT_BOTTOM,                  WIN_NOR40_LOWER_RIGHT_BOTTOM,
+        QAZ_NONE_THUMBS, WIN_QAZ_LOWER_LEFT_THUMBS,   WIN_QAZ_LOWER_RIGHT_THUMBS, QAZ_NONE_THUMBS
+    ),
+
+    [7] = LAYOUT_wrapper(
+        WIN_NOR40_ADJUST_LEFT_TOP,                     WIN_NOR40_ADJUST_RIGHT_TOP,
+        WIN_NOR40_ADJUST_LEFT_MIDDLE,                  WIN_NOR40_ADJUST_RIGHT_MIDDLE,
+        WIN_NOR40_ADJUST_LEFT_BOTTOM,                  WIN_NOR40_ADJUST_RIGHT_BOTTOM,
+        QAZ_NONE_THUMBS, WIN_QAZ_ADJUST_LEFT_THUMBS,   WIN_QAZ_ADJUST_RIGHT_THUMBS, KC_NO, KC_NO, QK_BOOT
     ),
   };
