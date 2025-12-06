648531067e6db02e67f8647ff0a3c31a75fcea1a
Karbou <201307916+karbou12@users.noreply.github.com>
2025-12-06 15:35:39 +0900

    Refactored to move km_rgb_layers definition.

--- keyboards/green_keys/gravity_45/keymaps/karbou12/km_rgb.c
+++ keyboards/green_keys/gravity_45/keymaps/karbou12/km_rgb.c
@@ -15,6 +15,19 @@ const rgblight_segment_t PROGMEM km_layer8_layer[]   = RGBLIGHT_LAYER_SEGMENTS({
 const rgblight_segment_t PROGMEM km_capsword_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_RED});
 const rgblight_segment_t PROGMEM km_layerOFF_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_OFF});
 
+const rgblight_segment_t * const PROGMEM km_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    km_layer0_layer,
+    km_layer1_layer,
+    km_layer2_layer,
+    km_layer3_layer,
+    km_layer4_layer,
+    km_layer5_layer,
+    km_layer6_layer,
+    km_layer7_layer,
+    km_layer8_layer,
+    km_capsword_layer
+);
+
 #ifdef RGBLIGHT_LAYER_BLINK
 const rgblight_segment_t PROGMEM km_reset_layer[] =       RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_WHITE});
 const rgblight_segment_t PROGMEM km_turn_on_layer[] =     RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_GOLD});
@@ -28,17 +41,4 @@ const rgblight_segment_t * const PROGMEM km_blink_layers[] = RGBLIGHT_LAYERS_LIS
     km_set_default_layer
 );
 #endif
-
-const rgblight_segment_t * const PROGMEM km_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-    km_layer0_layer,
-    km_layer1_layer,
-    km_layer2_layer,
-    km_layer3_layer,
-    km_layer4_layer,
-    km_layer5_layer,
-    km_layer6_layer,
-    km_layer7_layer,
-    km_layer8_layer,
-    km_capsword_layer
-);
 #endif
