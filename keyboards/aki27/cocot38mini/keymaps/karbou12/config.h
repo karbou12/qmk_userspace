b2f74714be77f91c9320e7e5cb90e4d860e294b2
Karbou <201307916+karbou12@users.noreply.github.com>
2025-12-09 23:04:39 +0900

    Modified keymaps

--- keyboards/aki27/cocot38mini/keymaps/karbou12/config.h
+++ keyboards/aki27/cocot38mini/keymaps/karbou12/config.h
@@ -1,3 +1,7 @@
+// Copyright 2022 aki27 (@aki27kbd)
+// Copyright 2025 Tano Karbou (github: karbou12 / X: @karbou_12)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
 #define VIAL_KEYBOARD_UID {0x39, 0x9F, 0x74, 0xF9, 0x88, 0x68, 0x7C, 0xFE}
 #define VIAL_UNLOCK_COMBO_ROWS {0, 0}
 #define VIAL_UNLOCK_COMBO_COLS {1, 9}
--- keyboards/aki27/cocot38mini/keymaps/karbou12/keymap.c
+++ keyboards/aki27/cocot38mini/keymaps/karbou12/keymap.c
@@ -14,10 +14,12 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+// Copyright 2025 Tano Karbou (github: karbou12 / X: @karbou_12)
 
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 #include "quantum.h"
+#include "us_keymap.h"
 #include "us_keycodes.h"
 
 #define MS_BTN1 KC_MS_BTN1
@@ -27,68 +29,84 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define COCOT_SCROLL_INV_DEFAULT false
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+     /*
+      * ┌───┬───┬───┬───┬───┐             ┌───┬───┬───┬───┬───┐
+      * │   │   │   │   │   │             │   │   │   │   │   │
+      * ├───┼───┼───┼───┼───┤             ├───┼───┼───┼───┼───┤
+      * │   │   │   │   │   │             │   │   │   │   │   │
+      * ├───┼───┼───┼───┼───┤             ├───┼───┼───┼───┼───┤
+      * │   │   │   │   │   │             │   │   │   │   │   │
+      * └───┴───┼───┼───┼───┤ ┌───┐ ┌───┐ ├───┼───┼───┼───┴───┘
+      *         │   │   │   │ │ C │ │ E │ │   │   │   │
+      *         └───┴───┴───┘ └───┘ └───┘ └───┴───┴───┘
+      */
 
-    [0] = LAYOUT(
-        KC_Q,          KC_W,          KC_E,             KC_R,           KC_T,                                  KC_Y,           KC_U,          KC_I,          KC_O,           KC_P,
-        KC_A,          LALT_T(KC_S),  LCTL_T(KC_D),     LGUI_T(KC_F),   KC_G,                                  KC_H,           KC_J,          KC_K,          KC_L,           RCTL_T(KC_SCLN),
-        LSFT_T(KC_Z),  KC_X,          KC_C,             KC_V,           KC_B,                                  KC_N,           KC_M,          KC_COMM,       RALT_T(KC_DOT), RSFT_T(KC_SLSH),
-                                      KC_ESC,           MO(2),          LGUI_T(KC_TAB),  XXXXXXX, MS_BTN1,     RSFT_T(KC_SPC), MO(1),         KC_RGUI
+    [0] = LAYOUT_wrapper(
+        MAC_QAZ_BASE_LEFT_TOP,                    MAC_QAZ_BASE_RIGHT_TOP,
+        MAC_QAZ_BASE_LEFT_MIDDLE,                 MAC_QAZ_BASE_RIGHT_MIDDLE,
+        MAC_QAZ_BASE_LEFT_BOTTOM,                 MAC_QAZ_BASE_RIGHT_BOTTOM,
+        MAC_QAZ_BASE_LEFT_THUMBS, KC_NO, MS_BTN1, MAC_QAZ_BASE_RIGHT_THUMBS
     ),
-    [1] = LAYOUT(
-        KC_PSLS,       KC_P7,         KC_P8,            KC_P9,          KC_PMNS,                               XXXXXXX,        KC_BSPC,       KC_DEL,        KC_TAB,         KC_MINS,
-        KC_PAST,       KC_P4,         KC_P5,            KC_P6,          KC_PPLS,                               KC_LEFT,        KC_DOWN,       KC_UP,         KC_RIGHT,       RCTL_T(KC_ENT),
-        LSFT_T(KC_P0), KC_P1,         KC_P2,            KC_P3,          KC_PEQL,                               KC_HOME,        KC_PGDN,       KC_PGUP,       RALT_T(KC_END), KC_RSFT,
-                                      KC_SPC,           LT(3, KC_PDOT), LGUI_T(KC_BSPC), MO(5),   LGUI(KC_P0), XXXXXXX,        XXXXXXX,       XXXXXXX
+
+    [1] = LAYOUT_wrapper(
+        MAC_QAZ_RAISE_LEFT_TOP,                        MAC_QAZ_RAISE_RIGHT_TOP,
+        MAC_QAZ_RAISE_LEFT_MIDDLE,                     MAC_QAZ_RAISE_RIGHT_MIDDLE,
+        MAC_QAZ_RAISE_LEFT_BOTTOM,                     MAC_QAZ_RAISE_RIGHT_BOTTOM,
+        MAC_QAZ_RAISE_LEFT_THUMBS, KC_NO, LGUI(KC_P0), MAC_QAZ_RAISE_RIGHT_THUMBS
     ),
-    [2] = LAYOUT(
-        LSFT(KC_SLSH), LSFT(KC_QUOT), LSFT(KC_1),       KC_BSPC,        LSFT(KC_GRAVE),                        XXXXXXX,        LSFT(KC_MINS), LSFT(KC_9),    LSFT(KC_0),     LSFT(KC_5),
-        LSFT(KC_2),    KC_QUOT,       LSFT(KC_4),       KC_DEL,         KC_SLSH,                               LSFT(KC_3),     XXXXXXX,       KC_LBRC,       KC_RBRC,        XXXXXXX,
-        XXXXXXX,       KC_GRAVE,      LSFT(KC_6),       LSFT(KC_BSLS),  KC_BSLS,                               LSFT(KC_7),     XXXXXXX,       LSFT(KC_LBRC), LSFT(KC_RBRC),  KC_RSFT,
-                                      XXXXXXX,          XXXXXXX,        XXXXXXX,         MO(5),   XXXXXXX,     RSFT_T(KC_ENT), MO(3),         XXXXXXX
+
+    [2] = LAYOUT_wrapper(
+        MAC_QAZ_LOWER_LEFT_TOP,                  MAC_QAZ_LOWER_RIGHT_TOP,
+        MAC_QAZ_LOWER_LEFT_MIDDLE,               MAC_QAZ_LOWER_RIGHT_MIDDLE,
+        MAC_QAZ_LOWER_LEFT_BOTTOM,               MAC_QAZ_LOWER_RIGHT_BOTTOM,
+        MAC_QAZ_LOWER_LEFT_THUMBS, KC_NO, KC_NO, MAC_QAZ_LOWER_RIGHT_THUMBS
     ),
-    [3] = LAYOUT(
-        KC_MUTE,       KC_F7,         KC_F8,            KC_F9,          KC_F12,                                MS_WHLL,        MS_WHLD,       MS_WHLU,       MS_WHLR,        LSG(KC_5),
-        KC_VOLU,       KC_F4,         KC_F5,            KC_F6,          KC_F11,                                MS_LEFT,        MS_DOWN,       MS_UP,         MS_RGHT,        KC_BRIU,
-        KC_VOLD,       KC_F1,         KC_F2,            KC_F3,          KC_F10,                                XXXXXXX,        KC_RGUI,       KC_RCTL,       KC_RALT,        RSFT_T(KC_BRID),
-                                      LCTL(LGUI(KC_Q)), XXXXXXX,        XXXXXXX,         MO(5),   KC_MUTE,     MS_BTN1,        MS_BTN3,       MS_BTN2
+
+    [3] = LAYOUT_wrapper(
+        MAC_QAZ_ADJUST_LEFT_TOP,                      MAC_QAZ_ADJUST_RIGHT_TOP,
+        MAC_QAZ_ADJUST_LEFT_MIDDLE,                   MAC_QAZ_ADJUST_RIGHT_MIDDLE,
+        MAC_QAZ_ADJUST_LEFT_BOTTOM,                   MAC_QAZ_ADJUST_RIGHT_BOTTOM,
+        MAC_QAZ_ADJUST_LEFT_THUMBS, QK_BOOT, KC_MUTE, MAC_QAZ_ADJUST_RIGHT_THUMBS
     ),
-    [4] = LAYOUT(
-        XXXXXXX,       XXXXXXX,       XXXXXXX,          XXXXXXX,        XXXXXXX,                               XXXXXXX,        XXXXXXX,       XXXXXXX,       XXXXXXX,        XXXXXXX,
-        XXXXXXX,       XXXXXXX,       XXXXXXX,          XXXXXXX,        XXXXXXX,                               XXXXXXX,        XXXXXXX,       XXXXXXX,       XXXXXXX,        XXXXXXX,
-        XXXXXXX,       XXXXXXX,       XXXXXXX,          XXXXXXX,        XXXXXXX,                               MS_BTN1,        MS_BTN3,       MS_BTN2,       XXXXXXX,        XXXXXXX,
-                                      XXXXXXX,          SCRL_MO,        MS_BTN1,         MS_BTN2, XXXXXXX,     XXXXXXX,        XXXXXXX,       XXXXXXX
+
+    [4] = LAYOUT_wrapper(
+        QAZ_NONE,                              QAZ_NONE,
+        QAZ_NONE,                              MS_BTN1, MS_BTN2, MS_BTN3, KC_NO, KC_NO,
+        QAZ_NONE,                              QAZ_NONE,
+        KC_NO, SCRL_MO, MS_BTN1, KC_NO, KC_NO, KC_BTN2, KC_NO, KC_NO
     ),
-    [5] = LAYOUT(
-        XXXXXXX,       XXXXXXX,       XXXXXXX,          XXXXXXX,        XXXXXXX,                               XXXXXXX,        XXXXXXX,       XXXXXXX,       XXXXXXX,        XXXXXXX,
-        XXXXXXX,       XXXXXXX,       XXXXXXX,          XXXXXXX,        XXXXXXX,                               RM_NEXT,        RM_HUEU,       RM_SATU,       RM_VALU,        RM_SPDU,
-        XXXXXXX,       XXXXXXX,       XXXXXXX,          XXXXXXX,        XXXXXXX,                               RM_PREV,        RM_HUED,       RM_SATD,       RM_VALD,        RM_SPDD,
-                                      XXXXXXX,          MO(6),          MO(7),           RM_TOGG, XXXXXXX,     XXXXXXX,        XXXXXXX,       XXXXXXX    ),
-    [6] = LAYOUT(
-        XXXXXXX,       XXXXXXX,       XXXXXXX,          XXXXXXX,        XXXXXXX,                               XXXXXXX,        XXXXXXX,       XXXXXXX,       XXXXXXX,        XXXXXXX,
-        XXXXXXX,       XXXXXXX,       XXXXXXX,          XXXXXXX,        XXXXXXX,                               XXXXXXX,        XXXXXXX,       XXXXXXX,       XXXXXXX,        XXXXXXX,
-        XXXXXXX,       XXXXXXX,       XXXXXXX,          XXXXXXX,        XXXXXXX,                               XXXXXXX,        XXXXXXX,       XXXXXXX,       XXXXXXX,        XXXXXXX,
-                                      MO(5),            XXXXXXX,        MO(7),           RM_TOGG, XXXXXXX,     XXXXXXX,        XXXXXXX,       XXXXXXX    ),
-    [7] = LAYOUT(
-        XXXXXXX,       XXXXXXX,       XXXXXXX,          XXXXXXX,        XXXXXXX,                               XXXXXXX,        XXXXXXX,       XXXXXXX,       XXXXXXX,        XXXXXXX,
-        XXXXXXX,       XXXXXXX,       XXXXXXX,          XXXXXXX,        XXXXXXX,                               XXXXXXX,        XXXXXXX,       XXXXXXX,       XXXXXXX,        XXXXXXX,
-        XXXXXXX,       XXXXXXX,       XXXXXXX,          XXXXXXX,        XXXXXXX,                               XXXXXXX,        XXXXXXX,       XXXXXXX,       XXXXXXX,        XXXXXXX,
-                                      MO(5),            MO(6),          XXXXXXX,         QK_BOOT, XXXXXXX,     XXXXXXX,        XXXXXXX,       XXXXXXX    )
+
+    [5] = LAYOUT_wrapper(
+        QAZ_NONE,                      QAZ_NONE,
+        QAZ_NONE,                      QAZ_NONE,
+        QAZ_NONE,                      QAZ_NONE,
+        QAZ_NONE_THUMBS, KC_NO, KC_NO, QAZ_NONE_THUMBS
+    ),
+
+    [6] = LAYOUT_wrapper(
+        QAZ_NONE,                      QAZ_NONE,
+        QAZ_NONE,                      QAZ_NONE,
+        QAZ_NONE,                      QAZ_NONE,
+        QAZ_NONE_THUMBS, KC_NO, KC_NO, QAZ_NONE_THUMBS
+    ),
+
+    [7] = LAYOUT_wrapper(
+        QAZ_NONE,                      QAZ_NONE,
+        QAZ_NONE,                      QAZ_NONE,
+        QAZ_NONE,                      QAZ_NONE,
+        QAZ_NONE_THUMBS, KC_NO, KC_NO, QAZ_NONE_THUMBS
+    )
 };
-
-
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] =   { ENCODER_CCW_CW(MS_WHLD, MS_WHLU) },
-    [1] =   { ENCODER_CCW_CW(LGUI(KC_PMNS), LGUI(KC_PPLS)) },
-    [2] =   { ENCODER_CCW_CW(KC_BRID, KC_BRIU) },
-    [3] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [4] =   { ENCODER_CCW_CW(XXXXXXX, XXXXXXX) },
-    [5] =   { ENCODER_CCW_CW(RM_HUED, RM_HUEU) },
-    [6] =   { ENCODER_CCW_CW(RM_SATD, RM_SATU) },
-    [7] =   { ENCODER_CCW_CW(RM_VALD, RM_VALU) },
+    [0] = ENCODER_MS_WHEEL,
+    [1] = ENCODER_MAC_ZOOM,
+    [2] = ENCODER_BRIGHT,
+    [3] = ENCODER_VOL,
+    [4] = ENCODER_MS_WHEEL,
+    [5] = ENCODER_WIN_ZOOM,
+    [6] = ENCODER_BRIGHT,
+    [7] = ENCODER_VOL,
 };
 #endif
-
-
-
